be601adb604c9d85934ee56950d2349270ecf0c9
administrator <administrator@192.168.0.101>
2022-04-18 10:42:54 +0530

    Demo

---
+++ NativeApp/src/Native/Capabilities.java
@@ -0,0 +1,50 @@
+package Native;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.AutomationName;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+
+
+public class Capabilities {
+	
+	
+	
+
+	public static AndroidDriver<AndroidElement> capability() throws MalformedURLException {
+		DesiredCapabilities dc = new DesiredCapabilities();
+		
+        dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Nishchith");
+				
+		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+		
+		//dc.setCapability(MobileCapabilityType.APP, "pathof the app");
+		
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "io.appium.android.apis");
+		
+		dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "io.appium.android.apis.ApiDemos");
+		//This is not a madate capability in android 
+		//but when you try to automate IOS application on Iphone
+		//this is mandate
+		//This is for IOS 
+		//dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.IOS_XCUI_TEST);
+		//This is for Android
+		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
+		//if you want to make it defeeault
+		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.APPIUM);
+		
+//		
+		
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://0.0.0.0:4723/wd/hub"),dc);
+		return driver;
+
+	}
+
+}
---
+++ NativeApp/src/Native/Demo1.java
@@ -0,0 +1,61 @@
+package Native;
+
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.By;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+public class Demo1 extends Capabilities{
+
+	public static void main(String[] args) throws MalformedURLException, InterruptedException {
+		
+		AndroidDriver<AndroidElement> driver = capability();
+		//if i run this what will happen
+		driver.findElement(MobileBy.AccessibilityId("Preference")).click();
+		Thread.sleep(5000);
+		driver.findElement(MobileBy.AccessibilityId("3. Preference dependencies")).click();
+		//beacuse id is a locator of selenium so i can use By.id directly //
+		//i use mobile By only if it is android locator
+		Thread.sleep(3000);
+		driver.findElement(By.id("android:id/checkbox")).click();
+		//if i dont have accessibilty id and i cnnot use xpath as it is big
+		//so i can identify the element using text
+		//Whenever i am identifing the element with attribute not locator then i that case  i use uiautomator
+		//and uiSelector is a method i am using this to identify the elemnt with a attribute 
+	//	driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"WiFi settings\")")).click();
+		//or
+		//driver.findElementByAndroidUIAutomator("UiSelector().text(\"WiFi settings\")").click();
+		//or
+		driver.findElement(By.xpath("//*[@text='WiFi settings']")).click();
+		
+		driver.findElement(By.className("android.widget.EditText")).sendKeys("sunil");
+		//keyboard is comming
+		driver.hideKeyboard();
+		//This is an example of when twoelements have same locator value so differentiate them using index
+		//if i make the index as 0 it will click on cancel
+		//if i make the index as 1 it ill click on ok button
+		driver.findElements(By.className("android.widget.Button")).get(0).click();
+		//i want to go back //navigate back ? how will i do it
+		//App_switch is basically to switch between two apps.
+		//This command will take you back one step
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+		//it will take you to the home page 
+		//driver.pressKey(new KeyEvent(AndroidKey.HOME));
+		//i want to open notification 
+		driver.openNotifications();
+		Thread.sleep(3000);
+		//driver.findElement(By.id("Battery Saver")).click();
+		driver.findElement(MobileBy.AccessibilityId("Battery Saver")).click();
+		Thread.sleep(2000);
+		driver.findElement(MobileBy.AccessibilityId("Battery Saver")).click();
+		driver.pressKey(new KeyEvent(AndroidKey.BACK));
+		
+	}
+	}
+
+
